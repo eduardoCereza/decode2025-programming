4890581b8e6828deb709f6ab8a443c3b84e80c5c
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-08-01 17:43:40 -0300

    Código inicial para teleoperado

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOpMode_Teste.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TeleOpMode_Teste extends OpMode {
+
+    @Override
+    public void init(){
+
+    }
+
+    public void loop(){
+        
+    }
+}

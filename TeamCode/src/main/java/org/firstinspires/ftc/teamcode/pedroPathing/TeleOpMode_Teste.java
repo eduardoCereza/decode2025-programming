58cb9ec0845b32da31abcbef2ebc96e2a47b50a4
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-08-01 17:55:59 -0300

    desenvolvendo modo teleoperado

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOpMode_Teste.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOpMode_Teste.java
@@ -1,15 +1,47 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.util.Constants;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.sun.tools.javac.code.Attribute;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+
 
 public class TeleOpMode_Teste extends OpMode {
+    private Follower follower;
+    double x, y, turn;
+    private final Pose startPose = new Pose(0, 0, 0);
 
     @Override
     public void init(){
+        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
 
     }
 
+    public void start(){
+        follower.startTeleopDrive();
+    }
+
     public void loop(){
+        x = gamepad1.left_stick_x;
+        y = -gamepad1.left_stick_y;
+        turn = gamepad1.right_stick_x;
+
+        follower.setTeleOpMovementVectors(x, y, turn);
+        follower.update();
+
+        telemetry.addData("X", follower.getPose().getX());
+        telemetry.addData("Y", follower.getPose().getY());
+        telemetry.update();
+    }
+
+    public void stop(){
         
     }
 }

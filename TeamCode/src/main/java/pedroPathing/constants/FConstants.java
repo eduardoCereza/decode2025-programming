81ebe5ed330e5591e8e200b643414abedfa2f7f0
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-08-06 15:22:56 -0300

    importando FConstants

---
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -0,0 +1,21 @@
+package pedroPathing.constants;
+
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.localization.Localizers;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class FConstants {
+    static {
+
+        FollowerConstants.localizers = Localizers.DRIVE_ENCODERS;
+        FollowerConstants.leftFrontMotorName = "leftFront";
+        FollowerConstants.leftRearMotorName = "leftRear";
+        FollowerConstants.rightFrontMotorName = "rightFront";
+        FollowerConstants.rightRearMotorName = "rightRear";
+
+        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+    }
+}

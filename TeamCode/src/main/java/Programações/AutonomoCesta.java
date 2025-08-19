6983e637638d57306c56b99a30919b28c2b2a3d3
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-08-19 13:59:22 -0300

    criando autonomo da cesta

---
+++ TeamCode/src/main/java/Programações/AutonomoCesta.java
@@ -0,0 +1,16 @@
+package Programações;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name="Autonomous Cesta")
+public class AutonomoCesta extends OpMode {
+
+    public void init(){
+
+    }
+
+    public void loop(){
+
+    }
+}
